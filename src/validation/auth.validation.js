ce6092731ea521818828aaece226968322a0610f
Liasu Aanuoluwapo <liasu.olabayo@gmail.com>
2024-07-09 17:11:11 +0100

    Update auth.validation.js

--- src/validation/auth.validation.js
+++ src/validation/auth.validation.js
@@ -5,7 +5,7 @@ const registerSchema = Joi.object({
   lastName: Joi.string().required(),
   email: Joi.string().email().required(),
   password: Joi.string().required(),
-  phoneNumber: Joi.string(),
+  phone: Joi.string(),
 });
 
 const loginSchema = Joi.object({
