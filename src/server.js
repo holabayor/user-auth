4843c2bfaa0199bc23f0b338f13a4116e2a06ff8
Liasu Aanuoluwapo <liasu.olabayo@gmail.com>
2025-01-11 21:42:36 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,5 +8,5 @@ const server = http.createServer(app);
 
 server.listen(PORT, () => {
   connectDB();
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
